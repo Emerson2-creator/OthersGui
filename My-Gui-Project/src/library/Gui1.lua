4130af9a185162c33b55512298d85a544ac1e13d
Emerson2-creator <josenetoboito@gmail.com>
2025-02-21 23:11:31 +0100

    Update Gui1.lua

--- My-Gui-Project/src/library/Gui1.lua
+++ My-Gui-Project/src/library/Gui1.lua
@@ -1,31 +1,7 @@
--- This file serves as a library that provides simplified access to the functionalities defined in the raw GUI code.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/OthersGui/refs/heads/main/My-Gui-Project/src/raw-gui/Gui1.lua?token=GHSAT0AAAAAAC7H4VDAJL7CZIIST5XMFQS2Z5Y7EGA"))()
 
-local guiLibrary = {}
+--Add a new button
 
--- Function to load the raw GUI code
-function guiLibrary.loadRawGUI()
-    local rawCode = [[
-        -- This is where the raw GUI code would be loaded
-        -- Example: return loadstring(rawCode)()
-    ]]
-    return loadstring(rawCode)()
-end
-
--- Function to create a simple button
-function guiLibrary.createButton(label, onClick)
-    local button = {} -- Placeholder for button object
-    button.label = label
-    button.onClick = onClick
-    -- Additional button setup code would go here
-    return button
-end
-
--- Function to create a simple label
-function guiLibrary.createLabel(text)
-    local label = {} -- Placeholder for label object
-    label.text = text
-    -- Additional label setup code would go here
-    return label
-end
-
-return guiLibrary
+local Button = addButton("Click me", function()
+    print("Button clicked")
+end)
