2820a5498ad40399a6c48d838bf4a8d6ab49a230
Emerson2-creator <josenetoboito@gmail.com>
2025-02-22 00:35:25 +0100

    Update Gui1.lua

--- My-Gui-Project/src/library/Gui1.lua
+++ My-Gui-Project/src/library/Gui1.lua
@@ -1,16 +1,215 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/OthersGui/refs/heads/main/My-Gui-Project/src/raw-gui/Gui1.lua"))()
+local Emerson2Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/OthersGui/refs/heads/main/My-Gui-Project/src/raw-gui/Gui1.lua"))()
+local Emerson2 = Emerson2Library.new("Universal Vehicle Script", 5013109572)
 
---Variable
-local gui = library.new("My GUI")
-local page = gui:addPage("Main Page")
-local section = page:addSection("Main Section")
---Button
-section:addButton("Click Me", function()
-    print("Button clicked!")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local LocalPlayer = Players.LocalPlayer
+
+local function GetVehicleFromDescendant(Descendant)
+    return
+        Descendant:FindFirstAncestor(LocalPlayer.Name .. "\'s Car") or
+        (Descendant:FindFirstAncestor("Body") and Descendant:FindFirstAncestor("Body").Parent) or
+        (Descendant:FindFirstAncestor("Misc") and Descendant:FindFirstAncestor("Misc").Parent) or
+        Descendant:FindFirstAncestorWhichIsA("Model")
+end
+
+local function TeleportVehicle(CoordinateFrame)
+    local Parent = LocalPlayer.Character.Parent
+    local Vehicle = GetVehicleFromDescendant(LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid").SeatPart)
+    LocalPlayer.Character.Parent = Vehicle
+    local success, response = pcall(function()
+        return Vehicle:SetPrimaryPartCFrame(CoordinateFrame)
+    end)
+    if not success then
+        return Vehicle:MoveTo(CoordinateFrame.Position)
+    end
+end
+
+local vehiclePage = Emerson2:addPage("Vehicle", 8356815386)
+local usageSection = vehiclePage:addSection("Usage")
+local velocityEnabled = true
+usageSection:addToggle("Keybinds Active", velocityEnabled, function(v) velocityEnabled = v end)
+local flightSection = vehiclePage:addSection("Flight")
+local flightEnabled = false
+local flightSpeed = 1
+flightSection:addToggle("Enabled", false, function(v) flightEnabled = v end)
+flightSection:addSlider("Speed", 100, 0, 800, function(v) flightSpeed = v / 100 end)
+local defaultCharacterParent 
+RunService.Stepped:Connect(function()
+    local Character = LocalPlayer.Character
+    if flightEnabled == true then
+        if Character and typeof(Character) == "Instance" then
+            local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+            if Humanoid and typeof(Humanoid) == "Instance" then
+                local SeatPart = Humanoid.SeatPart
+                if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+                    local Vehicle = GetVehicleFromDescendant(SeatPart)
+                    if Vehicle and Vehicle:IsA("Model") then
+                        Character.Parent = Vehicle
+                        if not Vehicle.PrimaryPart then
+                            if SeatPart.Parent == Vehicle then
+                                Vehicle.PrimaryPart = SeatPart
+                            else
+                                Vehicle.PrimaryPart = Vehicle:FindFirstChildWhichIsA("BasePart")
+                            end
+                        end
+                        local PrimaryPartCFrame = Vehicle:GetPrimaryPartCFrame()
+                        Vehicle:SetPrimaryPartCFrame(CFrame.new(PrimaryPartCFrame.Position, PrimaryPartCFrame.Position + workspace.CurrentCamera.CFrame.LookVector) * (UserInputService:GetFocusedTextBox() and CFrame.new(0, 0, 0) or CFrame.new((UserInputService:IsKeyDown(Enum.KeyCode.D) and flightSpeed) or (UserInputService:IsKeyDown(Enum.KeyCode.A) and -flightSpeed) or 0, (UserInputService:IsKeyDown(Enum.KeyCode.E) and flightSpeed / 2) or (UserInputService:IsKeyDown(Enum.KeyCode.Q) and -flightSpeed / 2) or 0, (UserInputService:IsKeyDown(Enum.KeyCode.S) and flightSpeed) or (UserInputService:IsKeyDown(Enum.KeyCode.W) and -flightSpeed) or 0)))
+                        SeatPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+                        SeatPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+                    end
+                end
+            end
+        end
+    else
+        if Character and typeof(Character) == "Instance" then
+            Character.Parent = defaultCharacterParent or Character.Parent
+            defaultCharacterParent = Character.Parent
+        end
+    end
 end)
---Toggle
-section:addToggle("Enable Feature", false, function(state)
-    print("Toggle state:", state)
+
+local speedSection = vehiclePage:addSection("Acceleration")
+local velocityMult = 0.025
+speedSection:addSlider("Multiplier (Thousandths)", 25, 0, 50, function(v) velocityMult = v / 1000 end)
+local velocityEnabledKeyCode = Enum.KeyCode.W
+speedSection:addKeybind("Velocity Enabled", velocityEnabledKeyCode, function()
+    if not velocityEnabled then
+        return
+    end
+    while UserInputService:IsKeyDown(velocityEnabledKeyCode) do
+        task.wait(0)
+        local Character = LocalPlayer.Character
+        if Character and typeof(Character) == "Instance" then
+            local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+            if Humanoid and typeof(Humanoid) == "Instance" then
+                local SeatPart = Humanoid.SeatPart
+                if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+                    SeatPart.AssemblyLinearVelocity *= Vector3.new(1 + velocityMult, 1, 1 + velocityMult)
+                end
+            end
+        end
+        if not velocityEnabled then
+            break
+        end
+    end
+end, function(v) velocityEnabledKeyCode = v.KeyCode end)
+
+local decelerateSelection = vehiclePage:addSection("Deceleration")
+local qbEnabledKeyCode = Enum.KeyCode.S
+local velocityMult2 = 150e-3
+decelerateSelection:addSlider("Brake Force (Thousandths)", velocityMult2 * 1e3, 0, 300, function(v) velocityMult2 = v / 1000 end)
+decelerateSelection:addKeybind("Quick Brake Enabled", qbEnabledKeyCode, function()
+    if not velocityEnabled then
+        return
+    end
+    while UserInputService:IsKeyDown(qbEnabledKeyCode) do
+        task.wait(0)
+        local Character = LocalPlayer.Character
+        if Character and typeof(Character) == "Instance" then
+            local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+            if Humanoid and typeof(Humanoid) == "Instance" then
+                local SeatPart = Humanoid.SeatPart
+                if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+                    SeatPart.AssemblyLinearVelocity *= Vector3.new(1 - velocityMult2, 1, 1 - velocityMult2)
+                end
+            end
+        end
+        if not velocityEnabled then
+            break
+        end
+    end
+end, function(v) qbEnabledKeyCode = v.KeyCode end)
+
+decelerateSelection:addKeybind("Stop the Vehicle", Enum.KeyCode.P, function(v)
+    if not velocityEnabled then
+        return
+    end
+    local Character = LocalPlayer.Character
+    if Character and typeof(Character) == "Instance" then
+        local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+        if Humanoid and typeof(Humanoid) == "Instance" then
+            local SeatPart = Humanoid.SeatPart
+            if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+                SeatPart.AssemblyLinearVelocity *= Vector3.new(0, 0, 0)
+                SeatPart.AssemblyAngularVelocity *= Vector3.new(0, 0, 0)
+            end
+        end
+    end
 end)
 
+local springSection = vehiclePage:addSection("Springs")
+springSection:addToggle("Visible", false, function(v)
+    local Character = LocalPlayer.Character
+    if Character and typeof(Character) == "Instance" then
+        local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+        if Humanoid and typeof(Humanoid) == "Instance" then
+            local SeatPart = Humanoid.SeatPart
+            if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+                local Vehicle = GetVehicleFromDescendant(SeatPart)
+                for _, SpringConstraint in pairs(Vehicle:GetDescendants()) do
+                    if SpringConstraint:IsA("SpringConstraint") then
+                        SpringConstraint.Visible = v
+                    end
+                end
+            end
+        end
+    end
+end)
 
+repeat
+    task.wait(0)
+until game:IsLoaded() and game.PlaceId > 0
+
+if game.PlaceId == 3351674303 then
+    local drivingEmpirePage = Emerson2:addPage("Wayfort", 8357222903)
+    local dealershipSection = drivingEmpirePage:addSection("Vehicle Dealership")
+    local dealershipList = {}
+    for index, value in pairs(workspace:WaitForChild("Game"):WaitForChild("Dealerships"):WaitForChild("Dealerships"):GetChildren()) do
+        table.insert(dealershipList, value.Name)
+    end
+    dealershipSection:addDropdown("Dealership", dealershipList, function(v)
+        game:GetService("ReplicatedStorage").Remotes.Location:FireServer("Enter", v)
+    end)
+elseif game.PlaceId == 891852901 then
+    local greenvillePage = Emerson2:addPage("Greenville", 8360925727)
+elseif game.PlaceId == 54865335 then
+    local ultimateDrivingPage = Emerson2:addPage("Westover", 8360954483)
+elseif game.PlaceId == 5232896677 then
+    local pacificoPage = Emerson2:addPage("Pacifico", 3028235557)
+end
+
+local infoPage = Emerson2:addPage("Information", 8356778308)
+local discordSection = infoPage:addSection("Discord")
+discordSection:addButton(syn and "Join the Discord server" or "Copy Discord Link", function()
+    if syn then
+        syn.request({
+            Url = "http://127.0.0.1:6463/rpc?v=1",
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["Origin"] = "https://discord.com"
+            },
+            Body = game:GetService("HttpService"):JSONEncode({
+                cmd = "INVITE_BROWSER",
+                args = {
+                    code = "ENHYznSPmM"
+                },
+                nonce = game:GetService("HttpService"):GenerateGUID(false)
+            }),
+        })
+        return
+    end
+    setclipboard("https://www.discord.com/invite/ENHYznSPmM")
+end)
+
+local function CloseGUI()
+    Emerson2:toggle()
+end
+
+UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+    if not gameProcessedEvent and input.KeyCode == Enum.KeyCode.RightBracket then
+        CloseGUI()
+    end
+end)
