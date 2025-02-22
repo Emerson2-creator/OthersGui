29de7b4f8d018072315a846c745f7e15cbacf71a
Emerson2-creator <josenetoboito@gmail.com>
2025-02-22 18:05:53 +0100

    u

--- GUI/src/library/VehicleScript.lua
+++ GUI/src/library/VehicleScript.lua
@@ -1,5 +1,5 @@
 -- Carrega a biblioteca Emerson2Library a partir de uma URL e cria uma nova instância
--- Feito por BOITONETO (Eu)
+-- Feito por BOITONETO 
 local Emerson2Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/OthersGui/refs/heads/main/GUI/src/raw-gui-code/VehicleScript.lua"))()
 local Emerson2 = Emerson2Library.new("Universal Vehicle Script", 5013109572)
 
