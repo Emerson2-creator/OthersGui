7e5cb8570fda42019814f3de457ee0864648b9f2
Emerson Jose Boito Neto <josenetoboito@gmail.com>
2025-02-28 00:08:38 +0100

    Update UniversalVehicleScript.lua

--- Code/UniversalVehicleScript.lua
+++ Code/UniversalVehicleScript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/OthersGui/refs/heads/main/GUI/src/library/VehicleScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/UniversalGuiScript-Scripts/refs/heads/main/Vehicle/Gui.lua"))()
